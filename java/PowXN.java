d615a8d4152253f811f0b8a34e39b9f24c5ae886
wish <wishsyouhappy1017@gmail.com>
2015-05-28 21:54:18 -0500

    pow

--- java/PowXN.java
+++ java/PowXN.java
@@ -1,30 +1,18 @@
 package leetcode;
 public class PowXN {
-	public double pow(double x, int n){
-		   if(n == 0) return 1;
-		   if(n < 0) return 1/helper(x, -n);
-		   else return helper(x, n);
-	}
-		
-	public double helper(double x, int n){
-	    if(n == 1) return x;
-	    else if(n % 2 == 0){
-	        double result = pow(x, n/2);
-	        return result * result;
-	    } 
-	    else {
-	        double result = pow(x, n/2);
-	        return result * result * x;
-	    } 
-	}
-	
-	public double pow1(double x, int n){
-	    if (n == 0) return 1;
-	    double result = pow(x, n/2);
-	    if(n % 2 == 0) return result * result;
-	    else if(n % 2 == 1) return result * result * x;
-	    else return result * result / x;
-	}
+    public double myPow(double x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        double result = myPow(x, n/2);
+        if (n % 2 == 0) {
+            return result * result;
+        } else if (n % 2 == 1) {
+             return result * result * x;
+        } else {
+            return result * result / x;
+        }
+    }
 	   
 	public static void main(String[] args) {
 	   PowXN p = new PowXN();
